2d84c56bd7ba34d7af4e8273d33eea5a982bbc26
HAKAN <lolhakanss@gmail.com>
2022-07-07 20:06:33 +0300

    naber

---
+++ src/main/java/me/earth/phobos/features/modules/render/SlowSwing.java
@@ -0,0 +1,27 @@
+package me.earth.phobos.features.modules.render;
+/*import me.htrewrite.client.event.custom.player.PlayerUpdateEvent;
+import me.earth.phobos.features.modules.Module;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+
+
+
+public class SlowSwing extends Module {
+    public
+    SlowSwing ( ) {
+        super ( "SlowSwing" , "Swings slow" , Module.Category.RENDER , true , false , false );
+    }
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        mc.player.removePotionEffect(MobEffects.MINING_FATIGUE);
+    }
+
+    @EventHandler
+    private Listener<PlayerUpdateEvent> updateEventListener = new Listener<>(event -> {
+        mc.player.addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, 999, 6));
+    });
+}*/
